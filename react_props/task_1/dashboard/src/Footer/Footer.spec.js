e21e121526f44640ca231a90fdd1194eb204123f
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-08-24 20:42:12 +0200

    Refactor Footer.spec.js test to improve readability by formatting the copyright text assertion across multiple lines.

--- react_props/task_1/dashboard/src/Footer/Footer.spec.js
+++ react_props/task_1/dashboard/src/Footer/Footer.spec.js
@@ -1,9 +1,13 @@
-import {render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Footer from './Footer'
 import { getCurrentYear, getFooterCopy } from '../utils/utils'
 
 test('renders Footer', () => {
   render(<Footer />)
-  const p = screen.getByText(`copyright ${getCurrentYear()} - ${getFooterCopy(true)}`)
+
+  const p = screen.getByText(
+    `Copyright ${getCurrentYear()} - ${getFooterCopy(true)}`
+  )
+
   expect(p).toBeInTheDocument()
 })
