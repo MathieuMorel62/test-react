de7cdcf6f9dc6492a4a67bc6ec65cf438bced443
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-09-11 12:00:15 +0200

    Refactor CourseList component to improve JSX structure and enhance readability by reorganizing the header and body sections.

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -2,43 +2,45 @@ import CourseListRow from './CourseListRow';
 import WithLogging from '../HOC/WithLogging';
 
 function CourseList({ courses = [] }) {
-    return (
-        <div className='course-list-container'>
-            <table id="CourseList">
-                {courses.length === 0 ? (
-                    <tbody>
-                        <CourseListRow
-                            textFirstCell="No course available yet"
-                        />
-                    </tbody>
-                ) : (
-                    <>
-                        <thead>
-                            <CourseListRow
-                                textFirstCell="Available courses"
-                                isHeader={true}
-                            />
-                            <CourseListRow
-                                textFirstCell="Course name"
-                                textSecondCell="Credit"
-                                isHeader={true}
-                            />
-                        </thead>
+  return (
+    <div className="course-list-container">
+      <table id="CourseList">
+        {courses.length === 0 ? (
+          <thead>
+            <CourseListRow
+              textFirstCell="No course available yet"
+              isHeader={true}
+            />
+          </thead>
+        ) : (
+          <>
+            <thead>
+              <CourseListRow
+                textFirstCell="Available courses"
+                isHeader={true}
+              />
 
-                        <tbody>
-                            {courses.map((course) => (
-                                <CourseListRow
-                                    key={course.id}
-                                    textFirstCell={course.name}
-                                    textSecondCell={course.credit}
-                                />
-                            ))}
-                        </tbody>
-                    </>
-                )}
-            </table>
-        </div>
-    );
+              <CourseListRow
+                textFirstCell="Course name"
+                textSecondCell="Credit"
+                isHeader={true}
+              />
+            </thead>
+
+            <tbody>
+              {courses.map((course) => (
+                <CourseListRow
+                  key={course.id}
+                  textFirstCell={course.name}
+                  textSecondCell={course.credit}
+                />
+              ))}
+            </tbody>
+          </>
+        )}
+      </table>
+    </div>
+  );
 }
 
 export default WithLogging(CourseList);
